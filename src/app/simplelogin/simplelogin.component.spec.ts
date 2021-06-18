2c6ec5d5208f4af29886c361c78623d2e79b960b
Pawel <73057350+PawelPotter1@users.noreply.github.com>
2021-06-18 21:52:22 +0200

    Add files via upload

---
+++ src/app/simplelogin/simplelogin.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SimpleloginComponent } from './simplelogin.component';
+
+describe('SimpleloginComponent', () => {
+  let component: SimpleloginComponent;
+  let fixture: ComponentFixture<SimpleloginComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ SimpleloginComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SimpleloginComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/simplelogin/users.ts
@@ -0,0 +1,11 @@
+
+export const users = [
+    {"id":1,"login":"test1","password":"haslo1"},
+    {"id":2,"login":"test2","password":"haslo2"},
+    {"id":3,"login":"test3","password":"p@ssw0rd3"},
+    {"id":4,"login":"test4","password":"p@ssw0rd4"},
+    {"id":5,"login":"test5","password":"p@ssw0rd5"},
+    {"id":6,"login":"test6","password":"p@ssw0rd6"},
+    {"id":7,"login":"test7","password":"p@ssw0rd7"},
+    {"id":8,"login":"test8","password":"p@ssw0rd8"}
+]
